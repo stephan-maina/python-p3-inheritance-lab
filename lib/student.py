546bbb26a68b688909faab20741cf54ae5124d07
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 02:24:28 +0300

    completed

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,26 @@
-#!/usr/bin/env python
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
 
-from user import User
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, information):
+        self.knowledge.append(information)
+
+    def get_knowledge(self):
+        return self.knowledge
+
+# Example usage:
+student = Student("Stephan", "Maina")
+student.learn("Mathematics")
+student.learn("History")
+
+print(f"Student: {student.get_full_name()}")
+print(f"Knowledge: {', '.join(student.get_knowledge())}")
