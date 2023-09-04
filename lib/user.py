e7575dc755ba9fdc97e4291bb584e1888aa3e609
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 02:22:29 +0300

    completed

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python
-
 class User:
-    pass
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+# Example usage:
+user = User("Stephan", "Maina")
+print(f"User: {user.get_full_name()}")
